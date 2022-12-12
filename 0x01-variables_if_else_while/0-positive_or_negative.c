483a4b223ffee810639f7c01cb71d880b8a4191b
Lucky Solomon <luckysolex1@gmail.com>
2022-12-12 07:06:20 -0800

    be mindful of symbols

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,13 @@
-#include (stdlib.h)
-#include (time.h)
-#include (stdio.h)
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
 /**
- * main - Entry point
+ * main - determine if number is positive or negative
  *
  * Return: Always 0 (success)
  */
+
 int main(void)
 {
 	int n;
@@ -15,16 +16,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	
 	if (n == 0)
-	{
 		printf("%d is zero\n", n);
-	}
 	else if (n < 0)
-	{
 		printf("%d is negative\n", n);
-	}
 	else
-	{
 		printf("%d is positive\n", n);
-	}
+	
 	return(0);
 }	
