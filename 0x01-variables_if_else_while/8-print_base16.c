a0b660ca8ed423a0b468fb382f8bf49d434f87db
Lucky Solomon <luckysolex1@gmail.com>
2022-12-12 16:47:38 -0800

    corrections

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,13 +13,16 @@ int main(void)
 	while (i < 48)
 	{
 		if (i < 10)
+		{
 			putchar(i + '0');
+		}
 		else if (i > 41);
+		{
 			putchar(i - 10 + 'A');
+		}
 		i++;
 	}
 	putchar(10);
 
 	return (0);
 }
-
