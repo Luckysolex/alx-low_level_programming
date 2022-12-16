aa2957b4a5ac2fdda8ff247f70461dfae1093d7f
Lucky Solomon <luckysolex1@gmail.com>
2022-12-15 20:30:22 -0800

    my head aches

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: the integer to be printed
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	int negative = 0;
+	int digit;
+	int divisor;
+	int begin = 0;
+	int place = 10;
+
+	if (n < 0)
+	{
+		negative = 1;
+		n = n * -1;
+	}
+	while (place >= 0)
+	{
+		divisor = power(10, place);
+		digit = ((n / divisor) % 10);
+		if (digit == 0 && begin == 0)
+		{
+			place--;
+		}
+		else if (digit != 0 && begin == 0)
+		{
+			begin = 1;
+			if (negative == 1)
+				_putchar('-');
+			_putchar('0' + digit);
+			place--;
+		}
+		else
+		{
+			_putchar('0' + digit);
+			place--;
+		}
+	}
+	if (digit == 0 && divisor == 1)
+	{
+		_putchar(48);
+	}
+}
