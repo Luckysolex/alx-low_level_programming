74ef5243acb09ea2486a34184b58a2b1d4d6a1ee
Lucky Solomon <luckysolex1@gmail.com>
2023-01-10 15:45:29 -0800

    Float like a butterfly, sting like a bee

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,51 @@
+#include "main.h"
+/**
+ * _strlen - give the lenght of a string
+ * @s: the string
+ *
+ * Return: the lenght of a string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	return (i);
+}
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup - returns a pointer which contains a copy of
+ * the string given as a parameter
+ * @str: the string to print
+ *
+ * Return: a pointer to the duplicated string or NULL
+ * if insufficient memory was available
+ */
+char *_strdup(char *str)
+{
+	int i;
+	char *copy;
+
+	if (str == '\0')
+	{
+		return ('\0');
+	}
+	i = _strlen(str);
+	copy = malloc(sizeof(char) * i + 1);
+	if (copy == '\0')
+	{
+		return ('\0');
+	}
+	else
+	{
+		for (i = 0; str[i] != '\0'; i++)
+		{
+			copy[i] = str[i];
+		}
+	}
+	copy[i] != '\0';
+	return (copy);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+malloc, vilgrant and free
