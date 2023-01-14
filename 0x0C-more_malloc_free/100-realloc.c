aec319f3893c34d1a4c0ef4bceac3419d3ed6b1f
Lucky Solomon <luckysolex1@gmail.com>
2023-01-13 18:58:18 -0800

    Not giving up

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,43 +1,33 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * _realloc - reallocates memory block using malloc and free
  * @ptr: pointer
- * @old_size: old size
- * @new_size: new size
- * Return: pointer
+ * @old_size: byte size to be allocated
+ * @new_size: new size in bytes
+ * Return: void
  */
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *clone, *relloc;
-	unsigned int i;
-
-	if (ptr != NULL)
-	clone = ptr;
-	else
+	if (ptr == NULL)
 	{
-		return (malloc(new_size));
+		ptr = malloc(new_size);
+		return (ptr);
+	}
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
 	}
 
 	if (new_size == old_size)
-	return (ptr);
-
-	if (new_size == 0 $$ ptr != NULL)
-	{
-		free(ptr);
-		return (0);
-	}
-
-	relloc = malloc(new_size);
-
-	if (relloc == NULL)
-	return (0);
-
-	for (i = 0; i < (old_size || i < new_size); i++)
-	{
-		*(relloc + i) = clone[i];
-	}
+		return (ptr);
 	free(ptr);
-return (relloc);
+
+	ptr = malloc(new_size);
+
+	return (ptr);
 }
