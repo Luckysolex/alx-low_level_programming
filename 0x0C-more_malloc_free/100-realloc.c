7e0baac21bb1890a782208390c7456c326a00466
Lucky Solomon <luckysolex1@gmail.com>
2023-01-13 17:42:44 -0800

    reallocation

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _realloc - reallocates memory block using malloc and free
  * @ptr: pointer
@@ -6,6 +7,7 @@
  * @new_size: new size
  * Return: pointer
  */
+
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *clone, *relloc;
