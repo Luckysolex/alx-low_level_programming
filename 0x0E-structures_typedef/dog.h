e3af7c79989436beaccf3ad39a9e045f565cb1d9
Lucky Solomon <luckysolex1@gmail.com>
2023-01-16 19:47:14 -0800

    why won't this dog run

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -5,13 +5,16 @@
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
+ * Description: this struct is for dog
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char owner;
-} dog_t;
+};
+
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
