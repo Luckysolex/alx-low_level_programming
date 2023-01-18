6006f3a618ce859f0f9d364ebb7ff71b570fd4ec
Lucky Solomon <luckysolex1@gmail.com>
2023-01-17 19:50:25 -0800

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
-#ifndef PET
-#define PET
+#ifndef DOG_H
+#define DOG_H
+int _putchar(char c);
 /**
  * struct dog - define a new type of struct
  * @name: name of dog
@@ -17,4 +18,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-#endif
+#endif /* !DOG_H */
