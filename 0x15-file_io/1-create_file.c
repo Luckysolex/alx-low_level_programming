17074fcc2982cf25a5701f1722ab06487feb1867
Luckysolex <luckydefreespirit@gmail.com>
2023-02-08 09:33:56 -0800

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,10 +3,10 @@
 /**
  * create_file - creates a file
  * @filename: A pointer to the name of the file to create
- * @text_content: A pointer to a string to write to file
+ * @text_content: A pointer to a string to write to a file
  *
  * Return: If the function fails - -1
- * otherwise - 1
+ * Otherwise - 1
  */
 int create_file(const char *filename, char *text_content)
 {
@@ -17,12 +17,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (len = o; text_content[len];)
+		for (len = 0; text_content[len];)
 			len++;
 	}
 
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, tex_content, len);
+	w = write(o, text_content, len);
 
 	if (o == -1 || w == -1)
 		return (-1);
