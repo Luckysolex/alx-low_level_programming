ae56d0e497371452b7df0161891d524df655b9a0
Luckysolex <Luckysolex1@gmail.com>
2023-03-17 19:16:31 -0700

    This function sums all the data in a list

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,15 +1,15 @@
 #include "lists.h"
 /**
- * sum_dlistint - sums all the data of a list
- * @head: head of a list
+ * sum_dlistint - sum of all data of a list
+ * @head: head of the list
  *
- * Return: sum or 0 if the list is empty
+ * Return: the sum of all data in a list
  */
 int sum_dlistint(dlistint_t *head)
 {
 	int addition = 0;
 
-	while (head != NULL);
+	while (head != NULL)
 	{
 		addition = addition + head->n;
 		head = head->next;
